f62ad52d2d1a698066d91d4e878bf6c21125c990
sailesh <sailesharsenal@gmail.com>
2022-07-24 23:29:55 +0200

    changed style usng react native paper

--- complaintsapp/App.js
+++ complaintsapp/App.js
@@ -1,52 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, AppRegistry, Image, TouchableOpacity } from 'react-native';
+import { Provider as PaperProvider, TextInput } from 'react-native-paper';
+import { theme } from './App.style';
+import { LoginScreen } from './app/screens/login/login.screen';
 import Logo from './logo.js';
 
 const App = () => {
   return (
-    <View>
-      <Logo />
-      <Text style= {styles.text}>Complainer</Text>
-      <TouchableOpacity>
-            <Text style = {styles.button}>
-               Login
-            </Text>
-      </TouchableOpacity>
-      <TouchableOpacity>
-            <Text style = {styles.button}>
-               Sign Up
-            </Text>
-      </TouchableOpacity>
-    </View>
-   
+      
+      <PaperProvider theme={theme}>
+        <Logo />
+        <LoginScreen/>
+      </PaperProvider>
+     
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text:{
-    alignSelf: 'center',
-    marginTop: 20,
-    fontSize: 30
-  },
-  button: {
-    width: 100,
-    textAlign: 'center',
-    borderWidth: 0,
-    padding: 10,
-    borderColor: 'black',
-    backgroundColor: 'skyblue',
-    alignItems: 'center',
-    alignSelf: 'center', 
-    marginTop: 40,
-    borderRadius: 5
- }
-});
 
 export default App
