4aec1b8c0c9f3ab18ce92138f56f7f754a876e3c
sailesh <sailesharsenal@gmail.com>
2022-07-28 20:54:01 +0200

    header component and conversion to typescript

--- complaintsapp/app/screens/register/register.screen.tsx
+++ complaintsapp/app/screens/register/register.screen.tsx
@@ -1,6 +1,7 @@
-import React from "react";
-import { SafeAreaView, ScrollView, View } from "react-native";
+import React from 'react';
+import { SafeAreaView, ScrollView, View } from 'react-native';
 import { Appbar, Button, TextInput } from "react-native-paper";
+import { HeaderComponent } from "../../components/header/header.component";
 import { registerStyle } from './register.style';
 
 export const RegisterScreen = () => {
@@ -8,10 +9,7 @@ export const RegisterScreen = () => {
     return (
         <SafeAreaView>
             <ScrollView>
-                <Appbar>
-                    <Appbar.BackAction />
-                    <Appbar.Content title="Register" />
-                </Appbar>
+                <HeaderComponent title="Regsiter"/>
                 <View style={registerStyle.content}>
                     <TextInput label="Name" />
                     <TextInput label="Email" keyboardType="email-address" />
@@ -19,7 +17,6 @@ export const RegisterScreen = () => {
                     <TextInput label="Confirm password" secureTextEntry={true} right={<TextInput.Icon name="eye-off-outline" color={registerStyle.icon.color } />} />
                     <TextInput label="Phone number" keyboardType="phone-pad" />
                     <Button mode="contained" style={registerStyle.button}>Register</Button>
-
                 </View>
             </ScrollView>
         </SafeAreaView>
