623ac0118cf51e13f3189f2b23755570ac935c17
sailesh <sailesharsenal@gmail.com>
2022-08-05 12:06:21 +0200

    validation to email, password, disabling of passord recovery link

--- complaintsapp/app/screens/login/login.screen.tsx
+++ complaintsapp/app/screens/login/login.screen.tsx
@@ -1,6 +1,6 @@
 import { Formik } from 'formik';
 import React from 'react';
-import { Alert, SafeAreaView, View, Text } from 'react-native';
+import { Alert, SafeAreaView, View, Text, ScrollView } from 'react-native';
 import { Button, Card, TextInput } from 'react-native-paper';
 import { loginForm } from './login.form';
 import { loginStyle } from './login.style';
@@ -19,24 +19,64 @@ const LoginScreen = (props: LoginScreenProps) => {
     return (
         <SafeAreaView style={loginStyle.content}>
             <View style={loginStyle.view}>
-                <Card>
+            <ScrollView>
+            <Card>
                     <Card.Title title="Complainer App" titleStyle={loginStyle.cardTitle}></Card.Title>
                     <Card.Content>
                         <Formik initialValues={{email: "", password: ""}} onSubmit={login} validationSchema ={loginForm}>
-                            {({handleSubmit, handleChange, errors}) => (
+                            {({handleSubmit, handleChange, errors, setFieldTouched, touched, values}) => (
                                 <>
-                                <TextInput label="Email" keyboardType="email-address" onChangeText={handleChange('email')} />
-                                <Text style={{color: "white", backgroundColor: "red"}}> {errors.email} </Text>
-                                <TextInput label="Password" secureTextEntry={true} onChangeText={handleChange('password')} />
-                                <Button uppercase={false} style={loginStyle.cardButton}>Forgot Email/Password</Button>
-                                <Button onPress={handleSubmit} mode="contained" style={loginStyle.cardButton}>Login</Button>
-                                <Button onPress={register} style={loginStyle.cardButton}>Register</Button>
+                                <TextInput 
+                                    label="Email" 
+                                    keyboardType="email-address" 
+                                    onChangeText={handleChange('email')} 
+                                    onFocus={() => setFieldTouched('email')} />
+                                {
+                                    touched.email && errors.email?
+                                    <Text style={{color: "white", backgroundColor: "red"}}> 
+                                        {errors.email} 
+                                    </Text>
+                                    :null
+                                }
+                                <TextInput 
+                                    label="Password" 
+                                    secureTextEntry={true} 
+                                    onChangeText={handleChange('password')} 
+                                    onFocus={() => setFieldTouched('email')} />
+                                 {
+                                    touched.password && errors.password?
+                                    <Text style={{color: "white", backgroundColor: "red"}}> 
+                                        {errors.password} 
+                                    </Text>
+                                    :null
+                                }    
+                                <Button 
+                                    uppercase={false} 
+                                    style={loginStyle.cardButton}
+                                    disabled={values.email == '' || errors.email ? true : false}>
+                                    Forgot Email/Password
+                                </Button>
+                                <Button 
+                                    onPress={handleSubmit} 
+                                    mode="contained" 
+                                    style={loginStyle.cardButton}>
+                                    Login
+                                </Button>
+                                <Button 
+                                    onPress={register} 
+                                    style={loginStyle.cardButton}>
+                                    Register
+                                </Button>
                                 </>
                             )}
                                 
                         </Formik>
                      </Card.Content>
                 </Card>
+
+            </ScrollView>
+
+                
             </View>
         </SafeAreaView>
     )
